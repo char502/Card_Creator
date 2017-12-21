0952cd31206a63e237edcb5f11ff1bfceffd43cc
char502 <char502@hotmail.com>
2017-12-21 22:13:01 +0000

    Started Card creator

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-(function () {
+window.onload = cardApp;
+
+function cardApp () {
   'use strict';
   
-  // Your code goes here
+  console.log('hello test');
+var el = document.getElementById("createCard");
+console.log(el);
+el.addEventListener("click", cardCreator);
+ function cardCreator() {
+ 	alert('can you see me');
+ }
+
   
-})();
+};
